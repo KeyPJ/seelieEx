1ddd91ebb674e09b1a7ba5ccf5c6c525f916540a
KeyPJ <pengjian1994@gmail.com>
2021-07-11 15:25:12 +0800

    build 1.0.2

--- dist/query.d.ts
+++ dist/query.d.ts
@@ -1,2 +1,4 @@
-export declare function getCharacterName(queryName: string): string;
+export declare const getCharacterId: (queryName: string) => string;
+export declare const getWeaponId: (queryName: string) => string;
+export declare const getElementAttrName: (queryName: number) => string;
 //# sourceMappingURL=query.d.ts.map
--- dist/seelie.d.ts
+++ dist/seelie.d.ts
@@ -1,4 +1,7 @@
+import Goal = seelie.Goal;
 import { mohoyo } from "./@type/mohoyo";
 import CharacterDataEx = mohoyo.CharacterDataEx;
+export declare const getTotalGoal: () => Goal[];
+export declare const addCharacterGoal: (level_current: number, nameEn: String, name: string, type: string) => void;
 export declare function addCharacter(characterDataEx: CharacterDataEx): void;
 //# sourceMappingURL=seelie.d.ts.map
