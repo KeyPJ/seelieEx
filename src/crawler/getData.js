c9b847a420eeffcda41ef82fabd0faed106189a6
KeyPJ <pengjian1994@gmail.com>
2021-08-07 00:44:12 +0800

    update getData.js

--- src/crawler/getData.js
+++ src/crawler/getData.js
@@ -6,10 +6,21 @@ async function getPageData(page, url, selector) {
     await page.goto(url, {
         waitUntil: 'domcontentloaded'
     })
+
+    //等待加载完毕
     await page.waitForSelector("button.w-full")
-    let button = await page.$('button.w-full')
+
+    //设置中文
+    await page.evaluate(() => {
+        [].slice.call(document.querySelector('.underline').parentNode.children).forEach(a => {
+            if (a.innerText == '中文') a.click()
+        })
+    })
+
+    //获取数据
+    const button = await page.$('button.w-full')
     await button.click()
-    return await page.$$eval(selector, relativeList => relativeList.map(
+    const list = await page.$$eval(selector, relativeList => relativeList.map(
         relative => {
             const scr = relative.firstElementChild.firstElementChild.src
             const id = scr.match(/(\/([\w-]*)\.png)/)[2]
@@ -17,10 +28,13 @@ async function getPageData(page, url, selector) {
             return {id, name}
         }
     ))
+
+    //排序
+    return list.sort((a, b) => a.name.localeCompare(b.name, 'zh'))
 }
 
-let scrape = async () => {
-    const browser = await puppeteer.launch({headless: false, devtools: true})
+const scrape = async () => {
+    const browser = await puppeteer.launch({headless: true, devtools: false})
     const page = await browser.newPage()
     await page.evaluateOnNewDocument(() => { //在每个新页面打开前执行以下脚本
         const newProto = navigator.__proto__
@@ -30,13 +44,13 @@ let scrape = async () => {
             get: () => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.113 Safari/537.36",
         })
     })
-    let charactersUrl = 'https://seelie.inmagi.com/characters'
-    let charactersSelector = '.items-center>.relative'
+    const charactersUrl = 'https://seelie.inmagi.com/characters'
+    const charactersSelector = '.items-center>.relative'
     const characters = await getPageData(page, charactersUrl, charactersSelector)
     console.log(characters)
 
-    let weaponsUrl = 'https://seelie.inmagi.com/weapons'
-    let weaponsSelector = '.items-start>.relative'
+    const weaponsUrl = 'https://seelie.inmagi.com/weapons'
+    const weaponsSelector = '.items-start>.relative'
     const weapons = await getPageData(page, weaponsUrl, weaponsSelector)
     console.log(weapons)
 
@@ -46,10 +60,10 @@ let scrape = async () => {
 
 scrape().then((value) => {
     const {characters, weapons} = value
-    let characterFilePath = path.join(__dirname, '../data/character.json')
+    const characterFilePath = path.join(__dirname, '../data/character.json')
     fs.writeFileSync(characterFilePath, JSON.stringify(characters))
 
-    let weaponFilePath = path.join(__dirname, '../data/weapon.json')
+    const weaponFilePath = path.join(__dirname, '../data/weapon.json')
     fs.writeFileSync(weaponFilePath, JSON.stringify(weapons))
 }).catch(
     // err => console.error(err)
