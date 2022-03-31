fc4a0b8d160565b62142d18d2e3937642db37b2f
KeyPJ <pengjian1994@gmail.com>
2022-03-31 15:15:07 +0800

    fix: draggable prevent click
    拖拽避免点击事件

--- src/App.tsx
+++ src/App.tsx
@@ -21,16 +21,30 @@ function App() {
         const {x, y} = data;
         localStorage.setItem("seelieExPosition", JSON.stringify({x, y}));
         setPosition({x, y})
+        eventControl(e, data)
+    }
+
+    const [isDragging, setIsDragging] = useState<any>(false);
+
+    const eventControl = (event: { type: any; }, info: any) => {
+        if (event.type === 'mousemove' || event.type === 'touchmove') {
+            setIsDragging(true)
+        }
+        if (event.type === 'mouseup' || event.type === 'touchend') {
+            setTimeout(() => {
+                setIsDragging(false);
+            }, 100);
+        }
     }
 
     return (
         <div className="App">
-            <Draggable nodeRef={nodeRef} defaultPosition={position} onStop={handleDragStop}>
+            <Draggable nodeRef={nodeRef} defaultPosition={position} onStop={handleDragStop} onDrag={eventControl}>
                 <div ref={nodeRef}
                      className="fixed inset-0 max-w-max max-h-max flex items-center justify-center z-[1201]">
                     <button
                         type="button"
-                        onClick={() => setShowExDialog(!showExDialog)}
+                        onClick={() => !isDragging && setShowExDialog(!showExDialog)}
                         className="px-4 py-2 text-sm font-medium text-white bg-black rounded-md bg-opacity-20 hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
                     >
                         SeelieEX
